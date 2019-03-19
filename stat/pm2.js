6c24a47910c3e4210285446be9033bed11a4d242
Matias <Matias Russitto>
2019-03-19 09:47:35 -0300

    fix(deploy): pm2 generated by env

--- stat/pm2.js
+++ stat/pm2.js
@@ -2,27 +2,24 @@
 const pack = require('../package.json')
 
 const name = pack.name
+const env = process.env['NODE_ENV'] || 'local'
 
-const config = {
-  apps: [
-    {
-      name,
-      script: './server.js',
-      instances: 'max',
-      cwd: `/opt/nodejs/repos/${name}/current`,
-      env: {
-        PORT: pack['x-port'],
-        LOGGER: 'yes'
-      },
-      env_preprod: {
-        LOGGER: 'no'
-      },
-      env_prod: {
-        LOGGER: 'no'
-      }
-    }
-  ]
+const app = {
+  name,
+  script: './server.js',
+  instances: 'max',
+  env: {
+    PORT: pack['x-port'],
+    LOGGER: env === 'prod' ? 'no' : 'yes'
+  }
+}
+
+if (env === 'prod') {
+  app.error_file = app.out_file = '/dev/null'
+}
+if (env !== 'local' && env !== 'docker') {
+  app.cwd = `/opt/nodejs/repos/${name}/current`
 }
 
 // eslint-disable-next-line no-console
-console.log(JSON.stringify(config, null, 2))
+console.log(JSON.stringify({ apps: [ app ] }, null, 2))
