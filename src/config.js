e63ed58e92d10ce97c94c416e276063aae371d7f
Gonzalo Spina <gjspina@teco.com.ar>
2019-08-22 16:21:25 -0300

    Removed loggly section from configuration file cause is not needed anymore

--- src/config.js
+++ src/config.js
@@ -5,10 +5,5 @@ const prefix = `/${pack.config.prefix}/v1`
 
 module.exports = {
   secret: process.env.SECRET || 'luego veremos algo mejor 201807',
-  prefix,
-  loggly: {
-    api: 'http://logs-01.loggly.com/inputs/',
-    token: '2c52ac24-4b71-40fd-975b-4f31b92c0546',
-    tags: 'backend,base,' + env
-  }
+  prefix
 }
