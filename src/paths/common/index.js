13e3600b718006000bc00aa2d45e9a6ebd2d408e
Gabriel Pasquale <gpasquale@teco.com.ar>
2020-03-02 18:00:38 -0300

    feat(base): get revision endpoint added

--- src/paths/common/index.js
+++ src/paths/common/index.js
@@ -1,5 +1,7 @@
 const ping = require('./ping')
+const revision = require('./revision')
 
 module.exports = (fastify, opts, next) => {
   fastify.register(ping.handler, opts, next)
+  fastify.register(revision.handler, opts, next)
 }
