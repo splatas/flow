a2bd7e0139fc89af0983a8574a6132dee1ee5707
darolp <daropl12@gmail.com>
2020-07-28 10:42:32 -0300

    fix: fixing revision endpoint

--- src/revision/index.js
+++ src/revision/index.js
@@ -5,7 +5,7 @@ const pkg = require('../../static/revision.json')
 
 module.exports = async function (fastify, opts, next) {
   fastify.get('/revision', { schema }, async (request, reply) => {
-    return reply.type('application/json').send(pkg)
+    return reply.type('application/json').send(`"${pkg}"`)
   })
   next()
 }
