4ce7186ce7b54fbae1b393383d75b2cd913698de
darolp <daropl12@gmail.com>
2020-07-29 12:29:31 -0300

    fix: fixing deploy

--- static/shipitfile.js
+++ static/shipitfile.js
@@ -21,11 +21,12 @@ module.exports = shipit => {
     default: {
       keepReleases: 5,
       deleteOnRollback: true,
-      shallowClone: true,
+      shallowClone: false,
       updateSubmodules: true,
       strict: 'no',
       workspace,
       deployTo,
+      keepWorkspace: 'false',
       ignores: [
         '.git',
         'node_modules'
