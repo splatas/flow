be11ddd022d39e2c51f96db622b4ff11c1f51521
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-05-21 16:23:10 -0300

    feat: deco-code schema updated #FBE-95

--- src/deco-code/schema.js
+++ src/deco-code/schema.js
@@ -1,68 +1,37 @@
 'use strict'
 const schema = {
+  tags: ['retail'],
   summary: 'Generate Code of activation',
-  tags: ['deco-retail'],
-  responses: {
-    200: {
-      description: 'OK',
-      content: {
-        'application/json': {
-          schema: {
-            type: 'object',
-            properties: {
-              code: {
-                type: 'string',
-                description: 'ABC12'
-              },
-              exp: {
-                type: 'integer',
-                description: 'Tiempo de expiración en tiempo unix',
-                example: 1620668011
-              },
-              msg: {
-                type: 'string'
-              },
-              label: {
-                type: 'string'
-              }
-            }
-          },
-          examples: {
-            'New code': {
-              value: {
-                code: '1A2B8',
-                exp: 1620668011
-              }
-            },
-            'No retail': {
-              value: {
-                msg: 'no es deco retail',
-                label: 'NOT_RETAIL'
-              }
-            }
-          }
-        }
-      }
-    }
-  },
-  operationId: 'get-retail-v1-code',
-  description: 'Permite generar un codigo de activación a para un deco retail.',
+  description: 'Permite generar un codigo de activación a para un deco retail',
   querystring: {
-    schema: {
-      type: 'string',
-      example: '14:13:46:D1:E5:16'
-    },
     type: 'object',
     properties: {
       macAddress: {
-        type: 'string',
-        description: 'Mac address del dispositivo',
-      },
+        type: 'string'
+      }
     },
     required: [
       'macAddress'
     ],
+    example: {
+      macAddress: '14:13:46:D1:E5:16'
+    }
   },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        code: { type: 'string' },
+        exp: { type: 'string' },
+        msg: { type: 'string' },
+        label: { type: 'string' }
+      },
+      example: {
+        code: '1A2B8',
+        exp: 1620668011
+      }
+    }
+  }
 }
 
 module.exports = schema
