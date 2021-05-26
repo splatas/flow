a4dc433dd7337aeb466aa54430a7179f76afdb66
Gabriel Pasquale <pasqualeunq@gmail.com>
2021-05-26 12:24:46 -0300

    fix(config): database constants deleted

--- constants/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  DATABASE_USER: process.env.DATABASE_USER || 'retail',
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'retail',
-  DATABASE_DB: process.env.DATABASE_DB || 'src/retail.db',
-  DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
-  DATABASE_DIALECT: process.env.DATABASE_DIALECT || 'sqlite',
-}
